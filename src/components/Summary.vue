494c153b242150d60a3b44d2e0f0701b4b17d01a
tcrha <thomas@9bitbyte.com>
2019-07-30 14:06:54 +1000

    added props for summary; basic outline of summary page done

--- src/components/Summary.vue
+++ src/components/Summary.vue
@@ -1,10 +1,22 @@
 <template>
   <div class='summary'>
-    <b-container class='summary-container'>
-      <b-col>
-        <b-img src={{images.medium}} fluid alt={{name}}></b-img>
-      </b-col>
-    </b-container>
+    <b-card>
+      <b-media>
+        <b-img :src='images.medium' slot="aside" blank-color="#ccc" width="150" alt="placeholder"></b-img>
+
+        <h4 class="mt-0">
+          {{name}}
+        </h4>
+        <span class="text-sm">
+          Genre: {{ genre.join(', ') }} | Rating: {{rating}}
+        </span>
+
+        <p v-html="summary">
+        </p>
+
+        <a :href="url" class="btn btn-outline-primary" role="button" aria-pressed="true">Official Site</a>
+      </b-media>
+    </b-card>
   </div>
 </template>
 
--- src/main.js
+++ src/main.js
@@ -2,7 +2,6 @@ import '@babel/polyfill'
 import 'mutationobserver-shim'
 import Vue from 'vue'
 import './plugins/bootstrap-vue'
-import './plugins/bootstrap-vue'
 import App from './App.vue'
 
 Vue.config.productionTip = false
