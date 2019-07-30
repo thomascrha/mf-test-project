23f8e962bc6520705d91c999a1a831ae8d70cbf2
tcrha <thomas@9bitbyte.com>
2019-07-30 16:44:27 +1000

    added season cards; added episode card deck

--- src/components/Episodes.vue
+++ src/components/Episodes.vue
@@ -1,12 +1,84 @@
 <template>
- <div class="episodes">
-    {{episodes}}
+ <div class="episodes"> 
+  <b-card>
+    <b-button-group class="seasons-buttons">
+      <b-button variant="outline-primary" class="active">All</b-button>
+      <div v-for="season in seasons">
+        <b-button variant="outline-primary">
+          {{season}}
+        </b-button>
+      </div>
+    </b-button-group>
+  </b-card>
+
+
+  <div class="container-fluid">
+    <b-card-group deck>
+      <b-card 
+        no-body 
+        class="b1" 
+      >
+        <b-row no-gutters>
+          <b-col md="6">
+            <b-card-img src="https://picsum.photos/400/400/?image=20" 
+              class="rounded-circle"></b-card-img
+            >
+          </b-col>
+          <b-col md="6">
+            <b-card-body title="Horizontal Card">
+              <b-card-text>
+                This is a wider card with supporting text as a natural lead-in to additional content.
+                This content is a little bit longer.
+              </b-card-text>
+            </b-card-body>
+          </b-col>
+        </b-row>
+      </b-card>
+      <b-card 
+        no-body 
+        class="m1" 
+      >
+        <b-row no-gutters>
+          <b-col md="6">
+            <b-card-img src="https://picsum.photos/400/400/?image=20" 
+              class="rounded-circle"></b-card-img
+            >
+          </b-col>
+          <b-col md="6">
+            <b-card-body title="Horizontal Card">
+              <b-card-text>
+                This is a wider card with supporting text as a natural lead-in to additional content.
+                This content is a little bit longer.
+              </b-card-text>
+            </b-card-body>
+          </b-col>
+        </b-row>
+      </b-card>
+    </b-card-group>
+  </div>
  </div>
 </template>
 
 <script>
 export default {
   name: 'Episodes',
-  props: ['episodes']
+  props: ['episodes', 'seasons']
 }
 </script>
+
+<style>
+
+@media(max-width: 768px){
+      .card-deck .card{
+        flex: 0 0 45%;
+        margin: 20px auto;
+      }
+    }
+
+@media(max-width: 619px){
+	.card-deck .card{
+		width: 80%;
+	}
+
+}
+</style>
--- src/components/Show.vue
+++ src/components/Show.vue
@@ -1,13 +1,17 @@
 <template>
   <div class='show'>
   <Summary
-          :summary='summary.summary'
-          :url='summary.url'
-          :genre='summary.genres'
-          :name='summary.name'
-          :rating='summary.rating.average'
-          :images='summary.image'>
+    :summary='summary.summary'
+    :url='summary.url'
+    :genre='summary.genres'
+    :name='summary.name'
+    :rating='summary.rating.average'
+    :images='summary.image'>
   </Summary>
+  <Episodes 
+    :seasons='seasons'
+    :episodes='episodes'>
+  </Episodes>
   </div>
 </template>
 
@@ -28,9 +32,14 @@ export default {
     // allowing for the data to be provided to the nessecary components
     const episodes = this.json['_embedded']['episodes']
     const summary = this.json
+    var seasons = null 
+    for (const episode of episodes) {
+      var seasons = (episode.season > seasons) ? episode.season : seasons 
+    }
     delete summary['_embedded']
     return {
       episodes: episodes,
+      seasons: seasons, 
       summary: summary
     }
   }
